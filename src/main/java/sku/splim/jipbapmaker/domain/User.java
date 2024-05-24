e309785be6280e66f027612af4047b963d77ff0d
hiho1010 <heeho1998@gmail.com>
2024-05-24 17:39:39 +0900

    Feat: make User domain & set security
    - User.java를 만들었습니다. 생성일과 수정일을 자동으로 넣을 수 있게 했습니다.
    - SpringApplication에 어노테이션을 추가하여 User.java에서 어노테이션들이 동작하도록 만들었습니다.
    - .gradle에 spring security 의존성을 추가했습니다.

---
+++ src/main/java/sku/splim/jipbapmaker/domain/User.java
@@ -0,0 +1,124 @@
+package sku.splim.jipbapmaker.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "User")
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(length = 255)
+    private String profile;
+
+    @Column(nullable = false)
+    private boolean enabled;
+
+    @Column(nullable = false)
+    private boolean push;
+
+    @Column(nullable = false)
+    private boolean log;
+
+    @Column(columnDefinition = "TEXT")
+    private String fcmToken;
+
+    @Column(columnDefinition = "TEXT")
+    private String accessToken;
+
+    @Column(columnDefinition = "TEXT")
+    private String refreshToken;
+
+    @CreatedDate
+    @Column(columnDefinition = "TEXT", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(columnDefinition = "TEXT")
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User(Long id, String email, String password, String nickname, String profile,
+                boolean enabled, boolean push, boolean log,
+                String fcmToken, String accessToken, String refreshToken,
+                LocalDateTime createdAt, LocalDateTime updatedAt) {
+
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.profile = profile;
+        this.enabled = enabled;
+        this.push = push;
+        this.log = log;
+        this.fcmToken = fcmToken;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    @Override // 권한 반환
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override // 사용자 id 반환
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
