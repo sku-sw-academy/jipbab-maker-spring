9ce25dbc68bac0ca5e87af13b665c8dff9c58e19
junhyunlim73 <limjh0703@gmail.com>
2024-06-07 11:17:30 +0900

    modify repository

--- src/main/java/sku/splim/jipbapmaker/repository/AnswerRepository.java
+++ src/main/java/sku/splim/jipbapmaker/repository/AnswerRepository.java
@@ -2,7 +2,11 @@ package sku.splim.jipbapmaker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sku.splim.jipbapmaker.domain.Answer;
+import sku.splim.jipbapmaker.domain.Log;
 import sku.splim.jipbapmaker.domain.Price;
 
+import java.util.List;
+
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findAllByOrderByModifyDateDesc();
 }
--- src/main/java/sku/splim/jipbapmaker/repository/QuestionRepository.java
+++ src/main/java/sku/splim/jipbapmaker/repository/QuestionRepository.java
@@ -1,8 +1,10 @@
 package sku.splim.jipbapmaker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import sku.splim.jipbapmaker.domain.Price;
 import sku.splim.jipbapmaker.domain.Question;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findAllByOrderByModifyDateDesc();
 }
