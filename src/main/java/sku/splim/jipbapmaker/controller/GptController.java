cfc2b4fb0248f3baa815569882a974ed5c3f5e6d
hiho1010 <heeho1998@gmail.com>
2024-06-18 14:52:47 +0900

    feat: "GPT 답변 향상"
    - GptController, GptService를 수정
    - system 값 수정
    - parsing 메서드 추가

--- src/main/java/sku/splim/jipbapmaker/controller/GptController.java
+++ src/main/java/sku/splim/jipbapmaker/controller/GptController.java
@@ -2,12 +2,10 @@ package sku.splim.jipbapmaker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import sku.splim.jipbapmaker.domain.Price;
 import sku.splim.jipbapmaker.dto.gpt.GptChatRequest;
 import sku.splim.jipbapmaker.dto.gpt.GptChatResponse;
 import sku.splim.jipbapmaker.service.GptService;
 import sku.splim.jipbapmaker.service.PreferenceService;
-import sku.splim.jipbapmaker.service.PriceService;
 
 import java.util.List;
 
@@ -16,13 +14,10 @@ import java.util.List;
 public class GptController {
     private final GptService gptService;
     private final PreferenceService preferenceService;
-    private final PriceService priceService;
-
     @Autowired
-    public GptController(GptService gptService, PreferenceService preferenceService, PriceService priceService) {
+    public GptController(GptService gptService, PreferenceService preferenceService) {
         this.gptService = gptService;
         this.preferenceService = preferenceService;
-        this.priceService = priceService;
     }
 
     @PostMapping("/recipe")
@@ -34,26 +29,10 @@ public class GptController {
                 String.join(", ", nonPreferences),
                 request.getThriftyItems());
 
-        // hard coding for prompt
-        String gptSetting = """
-                    Always respond in Korean.
-                    Use only the ingredients listed under 'preferences' and 'ThriftyItems',
-                    and never use any ingredients listed under 'non-preferences'.
-                    Based on this, create a recipe.
-                    The response format should be title : , content :.
-                    title에는 레시피 제목만 적어줘, content에는 조리법을 적어줘.
-                    """;
-        String gptAnswer = gptService.getGptAnswer(gptSetting, foodList);
-        int index = gptAnswer.indexOf("content : ");
+        String gptAnswer = gptService.getGptAnswer(foodList);
+        System.out.println(gptService.parseGptAnswer(gptAnswer));
 
-        String[] titleAndContent = gptAnswer.split(",");
-        String title = gptAnswer.substring(0, index-2);
-        String content = gptAnswer.substring(index);
-
-//        String title = titleAndContent[0];
-//        String content = titleAndContent[1];
-
-        return new GptChatResponse(title, content);
+        return gptService.parseGptAnswer(gptAnswer);
     }
 
 
--- src/main/java/sku/splim/jipbapmaker/service/GptService.java
+++ src/main/java/sku/splim/jipbapmaker/service/GptService.java
@@ -1,11 +1,15 @@
 package sku.splim.jipbapmaker.service;
 
+import com.theokanning.openai.OpenAiHttpException;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.service.OpenAiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sku.splim.jipbapmaker.config.GptProperties;
+import sku.splim.jipbapmaker.dto.gpt.GptChatResponse;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -13,10 +17,13 @@ import java.util.List;
 
 @Service
 public class GptService {
+    private static final Logger logger = LoggerFactory.getLogger(GptService.class);
 
     private final OpenAiService service;
 
-    private final static String GPT_MODEL = "gpt-3.5-turbo";
+    private final static String GPT_MODEL = "gpt-4o";
+
+    private final static double TEMPERATURE = 0.5;
 
     private static final Duration DURATION = Duration.ofSeconds(120);
 
@@ -25,12 +32,25 @@ public class GptService {
         this.service = new OpenAiService(gptProperties.getApiKey(), DURATION);
     }
 
-    private List<ChatMessage> getMessage(String content1, String content2) {
+    // Create a message to send to GPT
+    private List<ChatMessage> getMessage(String message) {
+
+        String system = """
+                        preferences: [사용자가 선호하는 재료 목록]
+                        non-preferences: [사용자가 선호하지 않는 재료 목록]
+                        thriftyItems: [사용자가 저렴하게 구할 수 있는 재료 목록]
+                        사용자가 제공한 재료를 사용하여 새로운 요리 레시피를 생성해주세요.
+                        레시피 제목은 이전 레시피 제목과 겹치지 않게 해주세요.
+                        모든 재료의 용량과 각 과정의 조리 시간을 포함하여 자세한 레시피를 작성해주세요.
+                        레시피는 한국어로 작성하고, 한 사람분량으로 만드세요. 절대 레시피 외에는 아무 말도 붙이지 말아주세요.
+                        답변은 무조건 이 형식을 따라해주세요.
+                        title: [recipe title] content: [recipe content]
+                        """;
 
         List<ChatMessage> messages = new ArrayList<>();
 
-        ChatMessage message1 = new ChatMessage("system", content1);
-        ChatMessage message2 = new ChatMessage("user", content2);
+        ChatMessage message1 = new ChatMessage("system", system);
+        ChatMessage message2 = new ChatMessage("user", message);
 
         messages.add(message1);
         messages.add(message2);
@@ -38,15 +58,47 @@ public class GptService {
         return messages;
     }
 
-    public String getGptAnswer(String content1, String content2) {
+    public String getGptAnswer(String message) {
         ChatCompletionRequest completionRequest = ChatCompletionRequest.builder()
-                .messages(getMessage(content1, content2))
+                .messages(getMessage(message))
                 .model(GPT_MODEL)
-                .temperature(0.2)
+                .temperature(TEMPERATURE)
                 .build();
 
-        // 받은 데이터
-        return service.createChatCompletion(completionRequest).getChoices().get(0).getMessage().getContent();
+        try {
+            // 받은 데이터
+            return service.createChatCompletion(completionRequest).getChoices().get(0).getMessage().getContent();
+        } catch (OpenAiHttpException e) {
+            if (e.getMessage().contains("You exceeded your current quota")) {
+                logger.error("API 사용 한도를 초과했습니다. 요금제 및 사용량을 확인해주세요.", e);
+            } else {
+                logger.error("OpenAiHttpException 발생: ", e);
+            }
+            return "오류가 발생했습니다. 관리자에게 문의하세요.";
+        } catch (Exception e) {
+            logger.error("알 수 없는 오류가 발생했습니다: ", e);
+            return "오류가 발생했습니다. 관리자에게 문의하세요.";
+        }
+    }
+
+    public GptChatResponse parseGptAnswer(String answer) {
+        // title: 이후의 부분 추출
+        String[] splitAnswer = answer.split("title: ");
+        String title = "";
+        String content = "";
+
+        if (splitAnswer.length > 1) {
+            // content: 이전까지의 부분을 title로 추출
+            String[] splitContent = splitAnswer[1].split("content: ");
+            title = splitContent[0].trim();
+            if (splitContent.length > 1) {
+                // content: 이후의 부분을 content로 추출
+                content = splitContent[1].trim();
+            }
+        }
+
+        // GptChatResponse 객체를 생성하여 반환
+        return new GptChatResponse(title, content);
     }
 
 }
